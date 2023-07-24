90420b3132f31afd75183fdd746b6ccee487ba85
Joshua Shunk <aerixia@gmail.com>
2023-07-24 08:15:13 -0400

    Add first comparison and move into one function

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -4,13 +4,14 @@
 
 // This creates an array and chooses a value based on a num between 0-1 (Math.random) multiplied by the number of items in the array (choices.length) then rounded down via the Math.floor 
 function getComputerChoice(){
-    let choices = ['rock', 'paper', 'scissors'];
-    const randomChoice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(randomChoice)
 }
 getComputerChoice();
 
 function getPlayerChoice(){
+    let choices = ['rock', 'paper', 'scissors'];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    console.log(computerChoice)
+    
     let playerChoice = prompt("Rock, paper, or scissors?");
     playerChoice = playerChoice.toLowerCase();
     if (playerChoice == 'rock' || playerChoice == 'paper' || playerChoice == 'scissors'){
@@ -20,8 +21,15 @@ function getPlayerChoice(){
             alert("Please enter a valid choice.");
             return;
         }
-
+        
+    if (playerChoice == 'rock' && computerChoice == 'rock'){
+        console.log("It's a tie!")
+    }
     }
 
 
 getPlayerChoice();
+
+// function playRound(){
+//     }
+// }
