b1714e1bf65b3ea6f0f161c6fc27a3cfe72d54f8
Joshua Shunk <aerixia@gmail.com>
2023-07-24 09:39:26 -0400

    Move scoring to be declared before loop and add score console text

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -2,19 +2,24 @@
 // change the case to lowercase to pass through the program
 // Generate a value of rock, paper, or scissors from the computer
 
-
-
-function playRound(){
-    var computerChoice;
-    var playerChoice;
+function game(){
     var playerScore = 0;
     var computerScore = 0;
     var tieCounter = 0;
+
+    for(i = 0; i < 5; i++){
+        playRound(i);
+    }
+    
+    
+    function playRound(){
+        var computerChoice;
+    var playerChoice;
 // This creates an array and chooses a value based on a num between 0-1 (Math.random) multiplied by the number of items in the array (choices.length) then rounded down via the Math.floor 
 function getComputerChoice(){
     const choices = ['rock', 'paper', 'scissors'];
     computerChoice = choices[Math.floor(Math.random() * choices.length)];
-    console.log(computerChoice)
+    // console.log(computerChoice)
     return computerChoice;
 }
 getComputerChoice();
@@ -24,7 +29,7 @@ function getPlayerChoice(){
     playerChoice = playerChoice.toLowerCase();
     if (playerChoice == 'rock' || playerChoice == 'paper' || playerChoice == 'scissors'){
         // alert("You chose " + playerChoice + ".")
-        console.log(playerChoice);
+        // console.log(playerChoice);
         return playerChoice;
     }
     else{
@@ -42,12 +47,12 @@ function gameLogic(){
     if (playerChoice == 'rock' && computerChoice == 'scissors'){
         console.log("Rock beats scissors, you win!");
         // add a score++ here for the player
-        playerScore++;
+        return playerScore++;
     }
     else if(playerChoice == 'rock' && computerChoice == 'paper'){
         console.log("Paper beats rock, you lose.");
         // add a score++ here for the computer
-        computerScore++;
+        computerScore++
     }
     else if(playerChoice == 'rock' && computerChoice == 'rock'){
         console.log("It's a tie!")
@@ -63,7 +68,7 @@ function gameLogic(){
     else if(playerChoice == 'scissors' && computerChoice == 'rock'){
         console.log("Rock beats scissors, you lose.");
         // add a score++ here for the computer
-        computerScore++;
+        computerScore++
     }
     else if(playerChoice == 'scissors' && computerChoice == 'scissors'){
         console.log("It's a tie!")
@@ -79,21 +84,22 @@ function gameLogic(){
     else if(playerChoice == 'paper' && computerChoice == 'scissors'){
         console.log("Scissors beats paper, you lose.");
         // add a score++ here for the computer
-        computerScore++;
+        computerScore++
     }
     else if(playerChoice == 'paper' && computerChoice == 'paper'){
         console.log("It's a tie!")
         // add a tie++ for a tie counter
         tieCounter++;
     }
-    console.log(playerScore);
-    console.log(computerScore);
-    console.log(tieCounter);
 }
 gameLogic();
 }
-
-playRound();
+console.log("Player score: " + playerScore);
+console.log("Computer score: " + computerScore);
+console.log("Tie rounds: " + tieCounter);
+}
+game();
+// playRound();
 
 // function playRound(){
 //     }
